4a5833b8da76cfdaf404d600a00e91f3b48706ba
Jaden Lawrence <jaden2.lawrence@live.uwe.ac.uk>
2024-11-05 00:07:58 +0000

    Added moving platform
    Added a moving platform that is controlled by typing directions in Twitch chat. The platform comes to a stop when it hits a wall

--- TwitchIntGame/Assets/Scripts/PlatformMovement.cs
+++ TwitchIntGame/Assets/Scripts/PlatformMovement.cs
@@ -1,23 +1,68 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class PlatformMovement : MonoBehaviour
 {
     private TwitchConnect twitch;
+
+    private Vector2 direction;
+    [SerializeField] private float moveSpeed = 5;
+
+    private GameObject map;
+    private Rigidbody2D rb;
+
     void Awake()
     {
         twitch = GameObject.Find("TwitchConnect").GetComponent<TwitchConnect>();
+        direction = new Vector2(0, 0);
+        map = GameObject.Find("Foreground");
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        rb.velocity = new Vector2(direction.x * moveSpeed, direction.y * moveSpeed);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision == map.GetComponent<Collision2D>())
+        {
+            rb.velocity = new Vector2(0, 0);
+        }
     }
 
     public void OnChatMessage()
     {
-        print(twitch.msg);
+        if (twitch.msg.ToLower().Contains("left"))
+        {
+            direction.x = -1;
+            direction.y = 0;
+            print(twitch.msg);
+        }
+
+        if (twitch.msg.ToLower().Contains("right"))
+        {
+            direction.x = 1;
+            direction.y = 0;
+            print(twitch.msg);
+        }
+
+        if (twitch.msg.ToLower().Contains("up"))
+        {
+            direction.x = 0;
+            direction.y = 1;
+            print(twitch.msg);
+        }
+
+        if (twitch.msg.ToLower().Contains("down"))
+        {
+            direction.x = 0;
+            direction.y = -1;
+            print(twitch.msg);
+        }
     }
 }
