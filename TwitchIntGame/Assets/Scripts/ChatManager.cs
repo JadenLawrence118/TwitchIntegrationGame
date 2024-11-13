6d2409b4c41d6449f236360518a8869ad60271f2
Jaden Lawrence <jaden2.lawrence@live.uwe.ac.uk>
2024-11-13 19:14:56 +0000

    Changed chatbox scroll
    Chatbox now scrolls to the bottom when number of messages surpasses max that can be shown on screen. This means the most recent messages will always be shown

--- TwitchIntGame/Assets/Scripts/ChatManager.cs
+++ TwitchIntGame/Assets/Scripts/ChatManager.cs
@@ -4,17 +4,32 @@ using Unity.VisualScripting;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UIElements;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class ChatManager : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI chatMessagePrefab;
     [SerializeField] CanvasGroup chatContent;
-    [SerializeField] Scroller scroller;
+    [SerializeField] ScrollRect scroller;
+
+    private void Awake()
+    {
+        scroller = GameObject.Find("Chat").GetComponent<ScrollRect>();
+    }
 
     public void AddMessage(string chatter, string msg)
     {
         TextMeshProUGUI message = Instantiate(chatMessagePrefab, chatContent.transform);
         message.SetText(chatter + ": " + msg);
         print(chatter + ": " + msg);
+        StartCoroutine(UpdateChat());
+    }
+
+    IEnumerator UpdateChat()
+    {
+        yield return new WaitForEndOfFrame();
+        print("hello");
+        scroller.verticalNormalizedPosition = 0;
     }
 }
