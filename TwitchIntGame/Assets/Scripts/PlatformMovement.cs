2a01bb4c42580b3259ce8c8c42004b4d7f18e795
Jaden Lawrence <jaden2.lawrence@live.uwe.ac.uk>
2024-11-17 22:27:15 +0000

    Stretchy platform (unfinished)
    Added the ability to stretch the platform. Currently does not have limits, so can shrink to a negative number, extending the opposite way. Also does not have Twitch Integration yet

--- TwitchIntGame/Assets/Scripts/PlatformMovement.cs
+++ TwitchIntGame/Assets/Scripts/PlatformMovement.cs
@@ -8,9 +8,13 @@ public class PlatformMovement : MonoBehaviour
     private Vector2 direction;
     [SerializeField] private float moveSpeed = 5;
 
+    private Vector2 stretch;
+    [SerializeField] private float stretchSpeed = 5;
+
     private GameObject map;
 
     private Vector2 lastPos;
+    private Vector2 lastScale;
 
     private Globals global;
 
@@ -18,13 +22,14 @@ public class PlatformMovement : MonoBehaviour
     {
         direction = new Vector2(0, 0);
         map = GameObject.Find("Foreground");
-        global = GameObject.Find("Globals").GetComponent<Globals>();
+        //global = GameObject.Find("Globals").GetComponent<Globals>();
     }
 
-    // Update is called once per frame
     void FixedUpdate()
     {
-        transform.position = new Vector2(transform.position.x + (direction.x * moveSpeed), transform.position.y + (direction.y * moveSpeed));
+        transform.parent.position = new Vector2(transform.position.x + (direction.x * moveSpeed), transform.position.y + (direction.y * moveSpeed));
+
+        transform.localScale = new Vector2(transform.localScale.x + (stretch.x * stretchSpeed), transform.localScale.y + (stretch.y * stretchSpeed));
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -33,12 +38,16 @@ public class PlatformMovement : MonoBehaviour
         {
             direction.x = 0;
             direction.y = 0;
-            transform.position = lastPos;
+            transform.parent.position = lastPos;
+
+            stretch.x = 0;
+            stretch.y = 0;
+            transform.localScale = lastScale;
         }
 
         if (collision.transform == GameObject.FindGameObjectWithTag("Player").transform)
         {
-            collision.transform.parent = transform;
+            collision.transform.parent = transform.parent;
         }
     }
 
@@ -52,29 +61,66 @@ public class PlatformMovement : MonoBehaviour
 
     private void Update()
     {
-        lastPos = transform.position;
+        lastPos = transform.parent.position;
+        lastScale = transform.localScale;
 
-        if (global.single)
+        //if (global.single)
         {
             if (Input.GetKeyUp(KeyCode.J))
             {
                 direction.x = -1;
                 direction.y = 0;
+                stretch.x = 0;
+                stretch.y = 0;
             }
             if (Input.GetKeyUp(KeyCode.L))
             {
                 direction.x = 1;
                 direction.y = 0;
+                stretch.x = 0;
+                stretch.y = 0;
             }
             if (Input.GetKeyUp(KeyCode.I))
             {
                 direction.x = 0;
                 direction.y = 1;
+                stretch.x = 0;
+                stretch.y = 0;
             }
             if (Input.GetKeyUp(KeyCode.K))
             {
                 direction.x = 0;
                 direction.y = -1;
+                stretch.x = 0;
+                stretch.y = 0;
+            }
+            if (Input.GetKeyUp(KeyCode.Y))
+            {
+                direction.x = 0;
+                direction.y = 0;
+                stretch.x = 1;
+                stretch.y = 0;
+            }
+            if (Input.GetKeyUp(KeyCode.U))
+            {
+                direction.x = 0;
+                direction.y = 0;
+                stretch.x = -1;
+                stretch.y = 0;
+            }
+            if (Input.GetKeyUp(KeyCode.O))
+            {
+                direction.x = 0;
+                direction.y = 0;
+                stretch.x = 0;
+                stretch.y = 1;
+            }
+            if (Input.GetKeyUp(KeyCode.P))
+            {
+                direction.x = 0;
+                direction.y = 0;
+                stretch.x = 0;
+                stretch.y = -1;
             }
         }
     }
